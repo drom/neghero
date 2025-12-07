8ed047b49cb0d6f8ee048b005b727e180c7d15e6
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2025-12-07 10:20:42 -0800

    first working code

--- README.md
+++ README.md
@@ -8,6 +8,16 @@ In 1+ layers.
 
 Idea is to generate continious path expanding from center with branches under 30 degrees.
 
+## Examples
+
+```js
+./bin/main.js -j "{tree:[7,[4,[5,[7,[5,[2,[5,[6,[6,[7,5,1],7],3],15],2],1],[9,[4,4,2],3]],5],5],3],s:4,l:11,p:2.8}" -o neghero-0005.gcode
+```
+
+![](./assets/heghero-0004.jpg)
+
+## TODO
+
 Online version: neghero.drom.io
 
 CLI version `npx neghero`
--- bin/main.js
+++ bin/main.js
@@ -1,16 +1,61 @@
 #!/usr/bin/env node
+'use strict';
 
-const generate = require('../lib/index.js');
+const fs = require('node:fs');
+const json5 = require('json5');
+const commander = require('commander');
+const {generate} = require('../lib/');
 
 const main = async () => {
-  const args = process.argv.slice(2);
-  const options = args.reduce((acc, arg) => {
-    const [key, value] = arg.split('=');
-    acc[key] = value;
-    return acc;
-  }, {});
-  const gcode = generate(options);
+  const program = new commander.Command();
+
+  program
+    .option('-c, --config <file>', 'config file (JSON5)')
+    .option('-j, --json <string>', 'config string (JSON5)')
+    .option('-p, --printer <file>', 'printer config file (JSON5)')
+    .option('-o, --output <file>', 'output G-code file')
+    .parse(process.argv);
+
+  const opts = program.opts();
+
+  opts.dna = { // default settings
+    s: 4, // scale
+    l: 4, // num layers
+    p: 2 // print line step
+  };
+
+  if (opts.config) {
+    const configBody = await fs.promises.readFile(opts.config, 'utf8');
+    const configObj = json5.parse(configBody);
+    Object.assign(opts.dna, configObj);
+  } else if (opts.json) {
+    const configObj = json5.parse(opts.json);
+    Object.assign(opts.dna, configObj);
+  } else {
+    program.help();
+    return;
+  }
+
+  opts.pcfg = { // printer settings
+    extrusionRate: 0.03326, // from my settings
+    xoffset: 250,
+    yoffset: 250
+  };
+
+  if (opts.printer) {
+    const printerBody = await fs.promises.readFile(opts.printer, 'utf8');
+    const printerObj = json5.parse(printerBody);
+    Object.assign(opts.pcfg, printerObj);
+  }
+
+  const gcode = generate(opts);
+
+  if (opts.output) {
+    await fs.promises.writeFile(opts.output, gcode);
+    return;
+  }
   console.log(gcode);
+
 };
 
 main();
